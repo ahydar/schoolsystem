9467d4c7604c4b75c1e77d5efb36b0fee75c0892
Abdullah Haydar <=>
2018-01-06 01:06:41 +0200

    Continue testing excel reader

--- public/read.php
+++ public/read.php
@@ -4,10 +4,23 @@ include "excelreader/excel_reader.php"; //including excel_reader class
 // creates an object instance of the class, and read the excel file data
 $excel = new PhpExcelReader;
 
-$filename = "History.xls";
+$filename = "Mathematics-11A.xls";
 $excel->read($filename);
 
-$sheet = $excel->sheets[2];
+$sheet = $excel->sheets[0];
 
-print_r($sheet);
+//print_r($sheet);
+for($i=0;$i<$sheet['numRows']-12;$i++){
+    if(isset($sheet['cells'][12+$i][2])){
+        echo $sheet['cells'][12+$i][1].": ";
+        echo $sheet['cells'][12+$i][2]."<br>";
+        echo $sheet['cells'][12+$i][27]."--";
+        echo $sheet['cells'][12+$i][28]."--";
+        echo $sheet['cells'][12+$i][29]."--";
+        echo $sheet['cells'][12+$i][30]."--";
+    
+        echo $sheet['cells'][12+$i][38]."<br><br>";
+    }
+   
+}
 ?>
