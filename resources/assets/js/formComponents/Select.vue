eaca9f17934218486ffd71501cbc487038096908
Abdullah Haydar <=>
2018-01-14 08:03:49 +0200

    Change select button

--- resources/assets/js/formComponents/Select.vue
+++ resources/assets/js/formComponents/Select.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="form-group">
         <label>{{title}} <span v-if="required" style="color:red;">*</span></label>
-         <select class="form-control selectpicker" :id="field" :name='field' v-model="form[field]" :title="title">
+         <select class="form-control" :id="field" :name='field' v-model="form[field]" :title="title">
         <option v-for="item in items" v-bind:value="item[names.value]">
             {{ item[names.display] }}
         </option>
@@ -14,13 +14,7 @@
 export default {
   props:['title','field',"form","items","names","required"],
   mounted(){
-      $('#'+this.field).selectpicker('val',this.form[this.field]);
-      /*console.log("Form Select");
-      console.log(this.form);
-      console.log(this.field);
-      console.log("-----------------");
-      console.log(this.form[this.field]);
-      console.log("-----------------");*/
+      //$('#'+this.field).selectpicker('val',this.form[this.field]);
   }
 }
 </script>
