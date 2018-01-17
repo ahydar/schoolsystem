e1380c607bdf2307a59a70bef67acdbb047b00d2
Abdullah Haydar <=>
2018-01-17 16:43:39 +0200

    Change ALT to EDU

--- resources/views/partials2/header.blade.php
+++ resources/views/partials2/header.blade.php
@@ -4,7 +4,7 @@
     <!-- Logo -->
     <a href="#/" class="logo">
       <!-- mini logo for sidebar mini 50x50 pixels -->
-      <span class="logo-mini"><b>A</b>LT</span>
+      <span class="logo-mini"><b>Edu</b></span>
       <!-- logo for regular state and mobile devices -->
       <span class="logo-lg"><b>Edu</b>System</span>
     </a>
